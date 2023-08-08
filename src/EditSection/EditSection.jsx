fb82a1233d0d26bdfe1e05831360c232f59ebbda
acchayon <adhikaryac81@gmail.com>
2023-08-09 00:01:21 +0600

    added by chayon

---
+++ src/EditSection/EditSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EditSection = () => {
+    return (
+        <div>
+            <h2>There is my edit section by chayon</h2>
+        </div>
+    );
+};
+
+export default EditSection;
