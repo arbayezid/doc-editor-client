da17a4424aa26a77c9810d5318b129f5ceb3481b
Ataullah Mesbah Fahim <mesbahataullah4@gmail.comm>
2023-08-09 00:55:44 +0600

    review api and json file create

---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+    return (
+        <div className="mt-10 text-center text-red-500">
+            <h1>Review Section........</h1>
+        </div>
+    );
+};
+
+export default Home;
