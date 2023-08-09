394c0255687911c79a9679ce62bfd100590d7304
acchayon <adhikaryac81@gmail.com>
2023-08-09 13:19:33 +0600

    added editsection and template section by Chayon

--- src/EditSection/EditSection.jsx
+++ src/EditSection/EditSection.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import PicPart from "./PicPart";
+import WritePart from "./WritePart";
 
 const EditSection = () => {
     return (
-        <div>
-            <h2>There is my edit section by chayon</h2>
+        <div className="mt-10">
+            <h2 className="text-center px-4 mb-5 text-3xl font-bold">Edit documents with LIkho</h2>
+            
+            <div className="grid  grid-cols-1 md:grid-cols-2 lg:grid-cols-2 items-center">
+                <PicPart></PicPart>
+                <WritePart></WritePart>
+            </div>
         </div>
     );
 };
---
+++ src/Home/Home.jsx
@@ -0,0 +1,13 @@
+import EditSection from "../EditSection/EditSection";
+import TemplateSection from "../Template/TemplateSection";
+
+const Home = () => {
+    return (
+        <div>
+            <EditSection></EditSection>
+            <TemplateSection></TemplateSection>
+        </div>
+    );
+};
+
+export default Home;
