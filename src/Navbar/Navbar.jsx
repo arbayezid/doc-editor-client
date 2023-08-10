bb6b58234b30e21adb7c7ae141f624dd20f02818
Mou <mahejabinmou.99@gmail.com>
2023-08-10 10:15:13 -0700

    color fixed

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className="navbar  z-10 bg-opacity-30 max-w-screen-xl  bg-violet-400 font-bold ">
+    <div className="navbar  z-10 bg-opacity-30 max-w-screen-xl  bg-violet-300 font-bold ">
       <div className="navbar-start">
         <div className="dropdown ">
           <label tabIndex={0} className="btn btn-ghost lg:hidden">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
           </label>
-          <ul tabIndex={0} className="  menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-300 rounded-box w-52">
+          <ul tabIndex={0} className="  menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-violet-200 rounded-box w-52">
             <li><Link>Home</Link></li>
             <li><Link>Template</Link></li>
             
@@ -39,7 +39,7 @@ const Navbar = () => {
           <li tabIndex={0} >
             <details>
               <summary>Convert</summary>
-              <ul className="p-2 bg-base-300 w-32">
+              <ul className="p-2 bg-violet-200 w-32">
               <li><a>Text to pdf</a></li>
                 <li><a>Word to pdf</a></li>
                 <li><a>Docs to pdf</a></li>
@@ -59,11 +59,11 @@ const Navbar = () => {
               <img src="https://i.ibb.co/V2RBzLh/ana1.jpg" />
             </div>
           </label>
-          <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-300 rounded-box w-52">
+          <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-violet-200 rounded-box w-52">
             <li>
               <a className="justify-between">
                 Profile
-                <span className="badge bg-base-300 ">New</span>
+                <span className="badge bg-violet-200 ">New</span>
               </a>
             </li>
             <li><a>Settings</a></li>
