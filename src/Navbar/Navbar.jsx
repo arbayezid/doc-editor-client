ea4e7c4e815fa03d7d00a55b3c616f7135109367
arbayezid <arbayezid0@gmail.com>
2023-08-13 00:46:44 +0600

    bg changes

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className="navbar z-10 bg-opacity-30 bg-violet-200 font-bold ">
+    <div className="navbar z-10 bg-opacity-30 bg-violet-300 font-bold ">
       <div className="navbar-start">
         <div className="dropdown">
           <label tabIndex={0} className="btn btn-ghost lg:hidden">
